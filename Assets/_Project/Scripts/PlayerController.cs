bf927743836d3186fe8fd2304082ee60398003b0
Chau Thanh Dat <chauthanhdat@192.168.2.34>
2025-06-04 08:03:57 +0700

    Import model

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     public float speed = 5f;
     public float rotationSpeed = 10f;
-    public FixedJoystick fixedJoystick;
+    public FloatingJoystick floatingJoystick;
 
     [Header("Components")]
     public Animator animator;
     public Rigidbody rb;
+
+    private bool isFiring = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Fire");
+        }
+    }
 
     private void FixedUpdate()
     {
@@ -18,16 +29,17 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        Vector3 direction = new Vector3(fixedJoystick.Horizontal, 0, fixedJoystick.Vertical);
+        Vector3 direction = new Vector3(floatingJoystick.Horizontal, 0, floatingJoystick.Vertical);
         direction.Normalize();
 
         if (direction.magnitude > 0.1f)
         {
+            Debug.LogError("Rotate");
             Quaternion targetRotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed);
         }
 
-        rb.linearVelocity = new Vector3(fixedJoystick.Horizontal * speed, rb.linearVelocity.y, fixedJoystick.Vertical * speed);
+        rb.linearVelocity = new Vector3(floatingJoystick.Horizontal * speed, rb.linearVelocity.y, floatingJoystick.Vertical * speed);
 
         animator.SetFloat("Velocity", direction.magnitude);
     }
